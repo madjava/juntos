6f00eb4b790ba5884f52a2e967e247a6d3b49a20
Felix Eyetan <felix.eyetan@thomsonreuters.com>
2018-06-09 19:21:24 +0100

    Update to sql schema

--- resource/create_tables.sql
+++ resource/create_tables.sql
@@ -3,7 +3,7 @@ CREATE DATABASE unconf_db;
 CREATE TABLE `unconf_db`.`sessions` (
   `id` int NOT NULL AUTO_INCREMENT,
   `session_title` VARCHAR(60),
-  `session_time` DATETIME,
+  `session_time` VARCHAR(60),
   `event_id` INT,
   PRIMARY KEY (`id`)
 );
@@ -12,7 +12,7 @@ CREATE TABLE `unconf_db`.`events` (
 	`id` int NOT NULL AUTO_INCREMENT,
 	`company_name` VARCHAR(60), 
 	`event_location` VARCHAR(60),
-    `event_date` DATETIME,
+    `event_date` VARCHAR(60),
 	PRIMARY KEY (`id`)
 );
 
